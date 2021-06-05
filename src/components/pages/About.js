cfa4f8a3aaca50250cc8a9df8a475c65ae379f0f
JZSweet <sweet_zhaozhao@live.cn>
2021-06-05 15:35:14 -0700

    update main page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Figure, Container, Row, Col } from 'react-bootstrap';
-// import JZa from "./JZA.png"
-// import JZb from "./JZB.png"
-import JZa from "./img/jz1.png";
-import JZb from "./img/jz2.png";
+import JZ from "./img/jz.png";
 
-const btn ={backgroundColor: '#2B4141'};
+const btn = { backgroundColor: '#2B4141' };
 const styleObj = {
   fontSize: 20,
   color: "#E0E1F5",
-  display: "inline-flex",    
+  display: "inline-flex",
   position: "relative",
   paddingTop: "20px",
 }
@@ -17,21 +14,13 @@ const styleObj = {
 const About = () => (
   <Container style={btn}>
     <Row className="justify-content-md-center" >
-      <div style = {styleObj}>Welcome to JZ's portfolio</div>
-      </Row>
-    {/* <Row className="justify-content-md-center">Here is somethings about me</Row> */}
+      <div style={styleObj}>Welcome to JZ's portfolio</div>
+    </Row>
     <Row>
       <Col>
         <Figure>
           <Figure.Image
-            src={JZa}
-          />
-        </Figure>
-      </Col>
-      <Col>
-      <Figure>
-          <Figure.Image
-            src={JZb}
+            src={JZ}
           />
         </Figure>
       </Col>
